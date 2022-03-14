c5b8aa2a96d2f95d9df141ec3084523644106d9b
Jayden LaCombe <jaydenjameslacombe@gmail.com>
2022-03-14 11:38:10 -0500

    fixed perceptionTest.java

---
+++ src/perceptionTest.java
@@ -0,0 +1,35 @@
+/*
+ Assignment 2 - Perception Test
+ Jayden LaCombe and Lance Hoffpauer
+ 3/14/2022
+ */
+
+import javax.swing.*;
+
+public class perceptionTest {
+
+    public static void main(String[] args) {
+
+//        String[] colors = {"red", "blue", "green", "yellow", "black"};
+//        String[] shapes = {"square", "circle", "star", "wavy", "plus"};
+//
+//        Random randGen = new Random();
+//        int color = randGen.nextInt(6);
+//        int shape = randGen.nextInt(6);
+//
+//        String imageName = "psi_cards/" + colors[color] + "AND" + shapes[shape] + ".png";
+//        ImageIcon icon = new ImageIcon(imageName);
+//        JOptionPane.showMessageDialog(null, "" + imageName, "image demo", 0, icon);
+
+        // Welcome Screen
+
+        ImageIcon welcome = new ImageIcon("psi_cards/welcome.png");
+
+
+
+
+
+    }
+}
+
+
