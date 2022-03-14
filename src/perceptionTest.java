7af983c80472008d237c05fdc154de0588674771
LanceHoffpauer <lancehoffpauer1@gmail.com>
2022-03-14 12:28:24 -0500

    Lance Test 1

--- src/perceptionTest.java
+++ src/perceptionTest.java
@@ -10,16 +10,18 @@ public class perceptionTest {
 
     public static void main(String[] args) {
 
-//        String[] colors = {"red", "blue", "green", "yellow", "black"};
-//        String[] shapes = {"square", "circle", "star", "wavy", "plus"};
-//
-//        Random randGen = new Random();
-//        int color = randGen.nextInt(6);
-//        int shape = randGen.nextInt(6);
-//
-//        String imageName = "psi_cards/" + colors[color] + "AND" + shapes[shape] + ".png";
-//        ImageIcon icon = new ImageIcon(imageName);
-//        JOptionPane.showMessageDialog(null, "" + imageName, "image demo", 0, icon);
+/*
+        String[] colors = {"red", "blue", "green", "yellow", "black"};
+        String[] shapes = {"square", "circle", "star", "wavy", "plus"};
+
+        Random randGen = new Random();
+        int color = randGen.nextInt(6);
+        int shape = randGen.nextInt(6);
+
+        String imageName = "psi_cards/" + colors[color] + "AND" + shapes[shape] + ".png";
+        ImageIcon icon = new ImageIcon(imageName);
+        JOptionPane.showMessageDialog(null, "" + imageName, "image demo", 0, icon);
+*/
 
         // Welcome Screen
 
