5a049350e3cf226f8058d4e4b1713864506fa684
Jayden LaCombe <jaydenjameslacombe@gmail.com>
2022-03-16 04:11:15 -0500

    Final Commit
    Full game is complete with everything functional.
    
    I added custom icons to replace ones given.

--- src/perceptionTest.java
+++ src/perceptionTest.java
@@ -5,203 +5,210 @@
  */
 
 import javax.swing.*;
-import java.util.*;
+import java.util.Objects;
+import java.util.Random;
 
 public class perceptionTest {
 
     public static void main(String[] args) {
 
-        // Local String Values
+        // String Values Used Throughout The Program
 
-        String stringTitle = "Jayden LaCombe and Lance Hoffpauer                                             Perception Test";
         String cardGenerated = ("Your card has been generated.");
-        String shapeQuestion = (cardGenerated + "\nWhat shape do you perceive?");
-        String colorQuestion = (cardGenerated + "\nWhat color do you perceive?");
+        String whatSuit = (cardGenerated + "\nWhat suit do you perceive?");
+        String whatColor = (cardGenerated + "\nWhat color do you perceive?");
 
-        String bothColorDialog = (cardGenerated + "\nYour first choice will be what color you perceive.");
-        String bothShapeDialog = ("Please enter your second choice. \nWhich shape do you perceive?");
-
-        String winningRound = ("Congrats! You were correct!");
-        String losingRound = ("Sorry, better luck next time.");
-
-        String[] colors = {"red", "blue", "green", "yellow", "black"};
-        String[] shapes = {"square", "circle", "star", "wavy", "plus"};
-
-        String[] next = {"Next"};
+        String[] colorList = {"red", "blue", "green", "yellow", "black"};
+        String[] suitList = {"spade", "club", "diamond", "heart", "joker"};
 
         // Images
 
         ImageIcon welcomeIcon = new ImageIcon("psi_cards/welcome.png");
-        ImageIcon questionIcon = new ImageIcon("psi_cards/question.png");
+        ImageIcon questionIcon = new ImageIcon("psi_cards/all-suits.png");
         ImageIcon unknownIcon = new ImageIcon("psi_cards/unknown.png");
 
         // Welcome Screen
 
-        String welcomeMessage = ("Welcome to the Perception Game!");
-        String[] welcomeButtons = {"Play", "Quit"};
+        String[] welcomeOptions = {"Play","Quit"};
 
-        int play = JOptionPane.showOptionDialog(null, welcomeMessage, stringTitle,
-                0, 0, welcomeIcon, welcomeButtons, welcomeButtons[0]);
+        int play = JOptionPane.showOptionDialog(null, "Welcome to the Perception Game!", "Perception Test",
+                JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, welcomeIcon, welcomeOptions, welcomeOptions[0]);
 
-        System.out.println(play);
+        if (play != 0) System.exit(0);
 
-        if (play == 0) {
+        int rightAnswers = 0;
+        int wrongAnswers = 0;
 
-            int rightAnswers = 0;
-            int wrongAnswers = 0;
+        // Play 5 More Loop
 
-            int roundNumber = 0;
+        int continueOrEndGame = 1;
+        int roundCounter = 0;
 
-            String[] endGame = {"Quit", "Play Another 5 Rounds"};
-            int endGameInt = 1;
+        while (continueOrEndGame == 1) {
 
-            while (Objects.equals(endGame[endGameInt], endGame[1])) {
+            // Choose Test Screen
 
-                // Choose Test Screen
+            String chooseTestString = ("Which perception test would you like to preform?");
+            String testString = ("");
+            String[] testButtons = {"suit", "Color", "Both"};
 
-                String chooseTestString = ("Which perception test would you like to preform?");
-                String testString = ("");
-                String[] testButtons = {"Shape", "Color", "Both"};
+            int testChosen = JOptionPane.showOptionDialog(null, chooseTestString, "Choose Test",
+                    JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, questionIcon, testButtons, testButtons[0]);
 
-                int testType = JOptionPane.showOptionDialog(null, chooseTestString, stringTitle,
-                        0, 0, questionIcon, testButtons, testButtons[0]);
+            switch (testChosen) {
+                case 0 -> testString = "suit";
+                case 1 -> testString = "Color";
+                case 2 -> testString = "Both";
+            }
 
-                switch (testType) {
-                    case 0 -> testString = "Shape";
-                    case 1 -> testString = "Color";
-                    case 2 -> testString = "Both";
-                }
+            int roundNumber = 1;
 
-                int placeholder = 0;
+            while (roundNumber <= 5) {
 
+                // Random Image Generator
 
+                Random randGen = new Random();
+                int randomColor = randGen.nextInt(5);
+                int randomSuit = randGen.nextInt(5);
 
-                    // Random Image Generator
+                String randomColorDialog = ("The color generated was -> " + colorList[randomColor]);
+                String randomSuitDialog = ("The suit generated was -> " + suitList[randomSuit]);
 
-                    Random randGen = new Random();
-                    int color = randGen.nextInt(5);
-                    int shape = randGen.nextInt(5);
+                String randomFileName = "psi_cards/" + colorList[randomColor] + "AND" + suitList[randomSuit] + ".png";
+                ImageIcon randomIcon = new ImageIcon(randomFileName);
 
-                    String randomColor = ("The color generated was -> " + colors[color]);
-                    String randomShape = ("The shape generated was -> " + shapes[shape]);
+                //
 
-                    String randomFileName = "psi_cards/" + colors[color] + "AND" + shapes[shape] + ".png";
-                    ImageIcon randomIcon = new ImageIcon(randomFileName);
+                String winningRound = ("Congrats! You were correct!");
+                String losingRound = ("Sorry, better luck next time.");
 
-                    switch (testString) {
-                        case "Shape" -> {
-                            String shapeResults = ("The shape you perceived -> ");
-                            int shapeRepeatInt = JOptionPane.showOptionDialog(null, shapeQuestion, stringTitle,
-                                    0, 0, unknownIcon, shapes, shapes[0]);
+                String[] next = {"Next"};
 
-                            shapeResults += (shapes[shapeRepeatInt] + "\n" + randomShape + "\n");
-                            if (Objects.equals(shapes[shapeRepeatInt], shapes[shape])) {
-                                shapeResults += winningRound;
-                                rightAnswers += 1;
-                            } else {
-                                shapeResults += losingRound;
-                                wrongAnswers += 1;
-                            }
+                switch (testString) {
+                    case "suit" -> {
+                        String suitResults = ("The suit you perceived -> ");
+                        int suitRepeatInt = JOptionPane.showOptionDialog(null, whatSuit, "Choose suit",
+                                JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, unknownIcon, suitList, suitList[0]);
 
-                            JOptionPane.showOptionDialog(null, shapeResults, stringTitle,
-                                    0, 0, randomIcon, next, next[0]);
-
-                        }
-                        case "Color" -> {
-                            String colorResults = ("The color you perceived -> ");
-                            int colorRepeatInt = JOptionPane.showOptionDialog(null, colorQuestion, stringTitle,
-                                    0, 0, unknownIcon, colors, colors[0]);
-
-                            colorResults += (colors[colorRepeatInt] + "\n" + randomColor + "\n");
-                            if (Objects.equals(colors[colorRepeatInt], colors[color])) {
-                                colorResults += winningRound;
-                                rightAnswers += 1;
-                            } else {
-                                colorResults += losingRound;
-                            }
-
-                            JOptionPane.showOptionDialog(null, colorResults, stringTitle,
-                                    0, 0, randomIcon, next, next[0]);
-
+                        suitResults += (suitList[suitRepeatInt] + "\n" + randomSuitDialog + "\n");
+                        if (Objects.equals(suitList[suitRepeatInt], suitList[randomSuit])) {
+                            suitResults += winningRound;
+                            rightAnswers += 1;
+                        } else {
+                            suitResults += losingRound;
+                            wrongAnswers += 1;
                         }
 
-                        case "Both" -> {
+                        int results = JOptionPane.showOptionDialog(null, suitResults, "Results",
+                                JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, randomIcon, next, next[0]);
 
-                            color = randGen.nextInt(5);
-                            shape = randGen.nextInt(5);
+                        if (results != 0) System.exit(0);
 
-                            randomColor = ("The color generated was -> " + colors[color]);
-                            randomShape = ("The shape generated was -> " + shapes[shape]);
-                            randomFileName = "psi_cards/" + colors[color] + "AND" + shapes[shape] + ".png";
-                            randomIcon = new ImageIcon(randomFileName);
+                        roundNumber += 1;
+                        roundCounter++;
 
-                            String colorResults = ("The color you perceived -> ");
-                            String shapeResults = ("The shape you perceived -> ");
+                    }
+                    case "Color" -> {
+                        String colorResults = ("The color you perceived -> ");
+                        int colorRepeatInt = JOptionPane.showOptionDialog(null, whatColor, "Choose Color",
+                                JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, unknownIcon, colorList, colorList[0]);
 
-
-                            int colorRepeatInt = JOptionPane.showOptionDialog(null, bothColorDialog, stringTitle,
-                                    0, 0, unknownIcon, colors, colors[0]);
-
-                            String repeatString = ("Your choice of color was " + "");
-                            String bothSecDialog = (repeatString + colors[colorRepeatInt] + "\n" + bothShapeDialog);
-
-                            int shapeRepeatInt = JOptionPane.showOptionDialog(null, bothSecDialog, stringTitle,
-                                    0, 0, unknownIcon, shapes, shapes[0]);
-
-                            String bothIfString = "";
-
-                            if (Objects.equals(colors[colorRepeatInt], colors[color]) & !Objects.equals(shapes[shapeRepeatInt], shapes[shape])) {
-
-                                bothIfString = (colorResults + colors[colorRepeatInt] + "\n" + randomColor + "\n" +
-                                        winningRound + "\n" + "Good perception on color!");
-                                rightAnswers += 1;
-                                bothIfString += ("\n\n" + shapeResults + shapes[shapeRepeatInt] + "\n" + randomShape + "\n" + "Not quite!");
-
-                            } else if (Objects.equals(shapes[shapeRepeatInt], shapes[shape]) & !Objects.equals(colors[colorRepeatInt], colors[color])) {
-
-                                bothIfString = (shapeResults + shapes[shapeRepeatInt] + "\n" + randomShape + "\n" +
-                                        winningRound + "\n" + "Good perception on shape!");
-                                rightAnswers += 1;
-                                bothIfString += ("\n\n" + colorResults + colors[colorRepeatInt] + "\n" + randomColor + "\n" + "Not quite!");
-
-                            } else if (Objects.equals(colors[colorRepeatInt], colors[color]) & Objects.equals(shapes[shapeRepeatInt], shapes[shape])) {
-
-                                bothIfString = (colorResults + colors[colorRepeatInt] + "\n" + randomColor + "\n" +
-                                        winningRound + "\n" + "Good perception on color!");
-                                rightAnswers += 1;
-                                bothIfString = (shapeResults + shapes[shapeRepeatInt] + "\n" + randomShape + "\n" +
-                                        winningRound + "\n" + "Good perception on shape!");
-                                rightAnswers += 1;
-                            } else {
-
-                                bothIfString += (shapeResults + shapes[shapeRepeatInt] + "\n" + randomShape + "\n" + "Not quite!\n");
-                                bothIfString += ("\n" + colorResults + colors[colorRepeatInt] + "\n" + randomColor + "\n" + "Not quite!");
-
-                            }
-
-                            JOptionPane.showOptionDialog(null, bothIfString, stringTitle,
-                                    0, 0, randomIcon, next, next[0]);
+                        colorResults += (colorList[colorRepeatInt] + "\n" + randomColorDialog + "\n");
+                        if (Objects.equals(colorList[colorRepeatInt], colorList[randomColor])) {
+                            colorResults += winningRound;
+                            rightAnswers += 1;
+                        } else {
+                            colorResults += losingRound;
+                            wrongAnswers++;
                         }
+
+                        int results = JOptionPane.showOptionDialog(null, colorResults, "Results",
+                                JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, randomIcon, next, next[0]);
+
+                        if (results != 0) System.exit(0);
+
+                        roundNumber += 1;
+                        roundCounter++;
 
                     }
 
-                roundNumber += 1;
+                    case "Both" -> {
 
-                if (roundNumber % 5 == 0) {
+                        String bothColorDialog = (cardGenerated + "\nYour first choice will be what color you perceive.");
+                        String bothSuitDialog = ("Please enter your second choice. \nWhich suit do you perceive?");
+                        String colorResults = ("The color you perceived -> ");
+                        String suitResults = ("The suit you perceived -> ");
 
-                    String correct = "Total Correct -> " + rightAnswers;
-                    String incorrect = "Total Incorrect -> " + wrongAnswers;
-                    String scoreboard = (correct + "\n" + incorrect + "\nWould you like to play again?");
 
+                        int colorRepeatInt = JOptionPane.showOptionDialog(null, bothColorDialog, "Choose Color",
+                                JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, unknownIcon, colorList, colorList[0]);
 
-                    endGameInt = JOptionPane.showOptionDialog(null, scoreboard, stringTitle,
-                            0, 0, unknownIcon, endGame, endGame[0]);
+                        String repeatString = ("Your choice of color was " + "");
+                        String bothSecDialog = (repeatString + colorList[colorRepeatInt] + "\n" + bothSuitDialog);
+
+                        int suitRepeatInt = JOptionPane.showOptionDialog(null, bothSecDialog, "Choose suit",
+                                JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, unknownIcon, suitList, 0);
+
+                        String bothIfString;
+
+                        String colorChosenUser = colorList[colorRepeatInt];
+                        String colorChosenRandom = colorList[randomColor];
+                        String suitChosenUser = suitList[suitRepeatInt];
+                        String suitChosenRandom = suitList[randomSuit];
+
+                        if (Objects.equals(colorChosenUser, colorChosenRandom) && (!Objects.equals(suitChosenUser, suitChosenRandom))) {
+
+                            bothIfString = (colorResults + colorChosenUser + "\n" + randomColorDialog + "\n" + "Good perception on color!\n\n");
+                            rightAnswers += 1;
+                            bothIfString += (suitResults + suitChosenUser + "\n" + randomSuitDialog + "\n" + "Not quite!");
+                            wrongAnswers += 1;
+
+                        } else if (!Objects.equals(colorChosenUser, colorChosenRandom) && (Objects.equals(suitChosenUser, suitChosenRandom))) {
+
+                            bothIfString = (suitResults + suitChosenUser + "\n" + randomSuitDialog + "\n" + "Good perception on suit!\n\n");
+                            rightAnswers += 1;
+                            bothIfString += (colorResults + colorChosenUser + "\n" + randomColorDialog + "\n" + "Not quite!\n\n");
+                            wrongAnswers += 1;
+
+                        } else if (Objects.equals(colorChosenUser, colorChosenRandom) && (Objects.equals(suitChosenUser, suitChosenRandom))) {
+
+                            bothIfString = (colorResults + colorChosenUser + "\n" + randomColorDialog + "\n"  + "Good perception on color!\n\n");
+                            rightAnswers += 1;
+                            bothIfString += (suitResults + suitChosenUser + "\n" + randomSuitDialog + "\n" + "Good perception on suit!");
+                            rightAnswers += 1;
+
+                        } else {
+
+                            bothIfString = (suitResults + suitChosenUser + "\n" + randomSuitDialog + "\n" + "Not quite!\n\n");
+                            wrongAnswers += 1;
+                            bothIfString += (colorResults + colorChosenUser + "\n" + randomColorDialog + "\n" + "Not quite!");
+                            wrongAnswers += 1;
+
+                        }
+
+                        int results = JOptionPane.showOptionDialog(null, bothIfString, "Results",
+                                JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, randomIcon, next, 0);
+
+                        if (results != 0) System.exit(0);
+
+                        roundNumber += 1;
+                        roundCounter++;
+
+                    }
 
                 }
             }
-        } else {
 
-            System.exit(0);
+            String[] endGame = {"Quit", "Play Another 5 Rounds"};
+            String roundCount = "Total Rounds Played -> " + roundCounter;
+            String correct = "Total Correct -> " + rightAnswers;
+            String incorrect = "Total Incorrect -> " + wrongAnswers;
+            String scoreboard = (roundCount + "\n" + correct + "\n" + incorrect + "\nWould you like to play again?");
+
+            continueOrEndGame = JOptionPane.showOptionDialog(null, scoreboard, "Game Over",
+                    JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, questionIcon, endGame, 0);
+
+            System.out.println(continueOrEndGame);
 
         }
     }
